d4b76454bd9bc46b764e577399342ed95a799708
harmain <harmainbhat.ba@gmail.com>
2026-04-17 12:32:50 +0530

    Add files via upload

---
+++ factor.c
@@ -0,0 +1,14 @@
+// code to print factors of a number given by user in c
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Factors of %d are: ", n);
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
---
+++ squareseries.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int num;
+printf("enter a number :");
+scanf("%d", &num);
+for(int i = 1; i <= num; i++){
+    printf("%d\n", i * i);
+}
+return 0;
+}
